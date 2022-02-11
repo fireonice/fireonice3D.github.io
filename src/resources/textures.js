fc138af44ff2bc3e407f25c01b9c516a46d1ecc7
gca <1637872839@qq.com>
2022-02-11 13:51:46 +0800

    deploy

---
+++ src/resources/textures.js
@@ -0,0 +1,61 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = 'src/jsm/blog.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture =
+  'src/jsm/music.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture =
+  'src/jsm/fund.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = 'src/jsm/githubLogo.png';
+boxTexture.BiliBili = 'src/jsm/BiliBili.png';
+boxTexture.QQ = 'src/jsm/qq.png';
+boxTexture.mail = 'src/jsm/envelope.png';
+boxTexture.reactIcon = 'src/jsm/react.png';
+boxTexture.allSkills = 'src/jsm/allSkills.png';
+boxTexture.lensFlareMain = 'src/jsm/lensflare0.png';
+boxTexture.skrillex = 'src/jsm/skrillex.png';
+boxTexture.edmText = 'src/jsm/EDM.png';
+
+// 砖块材质
+let stoneTexture = 'src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = 'src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = 'src/jsm/terp-solutions-text.png';
+inputText.activities = 'src/jsm/activities_text.png';
+inputText.bagholderBetsText = 'src/jsm/bagholderbets-text.png';
+inputText.homeSweetHomeText = 'src/jsm/home-sweet-home-text.png';
+inputText.staticPortfolio = 'src/jsm/static-portfolio.png';
+inputText.pcControl = 'src/jsm/pc-control.png'
+inputText.mobileControl = 'src/jsm/mobile-control.png'
+inputText.link = 'src/jsm/link.png'
+
+//SVG
+let SVG = {};
+SVG.reactLogo = 'src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog = 'https://fireonice.github.io/';
+URL.ryanfloyd = 'https://huabyte.com';
+URL.fund = '';
+URL.gitHub = 'https://github.com/fireonice';
+URL.BiliBili = 'https://www.bilibili.com/';
+URL.email = 'https://1637872839@qq.com';
+URL.music = 'https://music.qier222.com/';
+URL.devTo = 'https://fireonice.github.io/';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
